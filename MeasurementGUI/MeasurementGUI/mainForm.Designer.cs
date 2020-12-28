cff3ff8dea3cd15f40315c12c7a0464fc8f9b439
Maniaa <ibrahim.maniaa@siemens-healthineers.com>
2020-12-28 19:39:46 +0200

    Signed-off-by: Maniaa <ibrahim.maniaa@siemens-healthineers.com>

---
+++ MeasurementGUI/MeasurementGUI/mainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace MeasurementGUI
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.uploadImgBtn = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.zoomBtn = new System.Windows.Forms.Button();
+            this.homeBtn = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uploadImgBtn
+            // 
+            this.uploadImgBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uploadImgBtn.Location = new System.Drawing.Point(822, 37);
+            this.uploadImgBtn.Name = "uploadImgBtn";
+            this.uploadImgBtn.Size = new System.Drawing.Size(191, 46);
+            this.uploadImgBtn.TabIndex = 5;
+            this.uploadImgBtn.Text = "upload image";
+            this.uploadImgBtn.UseVisualStyleBackColor = true;
+            this.uploadImgBtn.Click += new System.EventHandler(this.uploadImgBtn_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold);
+            this.startBtn.Location = new System.Drawing.Point(822, 606);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(218, 147);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = true;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.saveBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveBtn.Image")));
+            this.saveBtn.Location = new System.Drawing.Point(167, 786);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(50, 50);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // zoomBtn
+            // 
+            this.zoomBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.zoomBtn.Image = ((System.Drawing.Image)(resources.GetObject("zoomBtn.Image")));
+            this.zoomBtn.Location = new System.Drawing.Point(89, 786);
+            this.zoomBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.zoomBtn.Name = "zoomBtn";
+            this.zoomBtn.Size = new System.Drawing.Size(50, 50);
+            this.zoomBtn.TabIndex = 2;
+            this.zoomBtn.UseVisualStyleBackColor = true;
+            // 
+            // homeBtn
+            // 
+            this.homeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.homeBtn.Image = ((System.Drawing.Image)(resources.GetObject("homeBtn.Image")));
+            this.homeBtn.Location = new System.Drawing.Point(11, 786);
+            this.homeBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.homeBtn.Name = "homeBtn";
+            this.homeBtn.Size = new System.Drawing.Size(50, 50);
+            this.homeBtn.TabIndex = 1;
+            this.homeBtn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(768, 768);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragDrop);
+            this.pictureBox.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox_DragEnter);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 843);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.homeBtn);
+            this.Controls.Add(this.zoomBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.uploadImgBtn);
+            this.Font = new System.Drawing.Font("Symbol", 8.25F);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadImgBtn;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button zoomBtn;
+        private System.Windows.Forms.Button homeBtn;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ MeasurementGUI/MeasurementGUI/mainForm.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeasurementGUI
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            this.Text = "MeasurementGUI";
+            pictureBox.AllowDrop = true;
+        }
+
+        private void uploadImgBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Image Files(*.png; *.jpg; *.jpeg; *.gif; *.bmp)|*.png; *.jpg; *.jpeg; *.gif; *.bmp",
+                Title = "Select image"
+            };
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = Image.FromFile(open.FileName);
+                this.Text = "";
+                this.Text += "MeasurementGUI -- " + open.FileName;
+                pictureBox.BackColor = this.BackColor;
+            }
+        }
+
+        private void pictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if(data != null)
+            {
+                var fileNames = data as string[];
+                if (fileNames.Length > 0)
+                {
+                    pictureBox.Image = Image.FromFile(fileNames[0]);
+                    
+                }
+            }
+        }
+
+        private void pictureBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(ImageFormat.Bmp.ToString()) &&
+            (e.AllowedEffect & DragDropEffects.Copy) != 0)
+            {
+                // Allow this.
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                // Don't allow any other drop.
+                e.Effect = DragDropEffects.None;
+            }
+            Console.WriteLine(" @@@@@@@ ");
+            Console.WriteLine(" @@@@@@@ " + e.Data.GetDataPresent(DataFormats.Bitmap));
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog
+                {
+                    Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|PNG Image|*.png",
+                    Title = "Save an Image File"
+                };
+
+                string[] subs = this.Text.Split('\\');
+                sfd.FileName = subs[subs.Length-1];
+
+                ImageFormat format = ImageFormat.Png;
+                if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (string.IsNullOrEmpty(sfd.FileName))
+                    {
+                        pictureBox.Image.Save(sfd.FileName, format);
+                    }
+                }
+            }
+            
+        }
+
+        Point startPos;         // mouse-down position
+        Point endPos;           // current mouse position
+        Point currentPos;
+        bool isDrawing = false;
+
+        private void partPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (imgSwitch)
+            {
+                startPos = e.Location;
+                isDrawing = true;
+            }
+        }
+
+        private void partPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                currentPos = e.Location;
+                partPictureBox.Paint += new PaintEventHandler(paintZoomRect);
+                partPictureBox.Refresh();
+            }
+        }
+
+
+        private bool isFirstZoom = false;
+        private void partPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                isDrawing = false;
+                partPictureBox.Paint += new PaintEventHandler(paintZoomRect);
+                partPictureBox.Refresh();
+
+                if (startPos == e.Location) { return; }
+                //else if (startPos.X - e.Location.X < 5 | startPos.Y - e.Location.Y < 5) { return; }
+                else { endPos = e.Location; }
+
+                //Console.WriteLine("?????  "+startPos.ToString() +"  " +currentPos.ToString() +"  " + endPos.ToString());
+
+                Bitmap bmp;
+                Rectangle cropRect = getZoomRect();
+
+                int xRatio = Convert.ToInt32(originPartPictureImg.Width / partPictureBox.Width);
+                int yRatio = Convert.ToInt32(originPartPictureImg.Height / partPictureBox.Height);
+
+                int margin = 0;
+                if (originPartPictureImg.Width > originPartPictureImg.Height) { margin = Convert.ToInt32((partPictureBox.Width - (originPartPictureImg.Width * (1 / xRatio))) / 2); }
+                else { margin = Convert.ToInt32((partPictureBox.Height - (originPartPictureImg.Height * (1 / yRatio))) / 2); }
+                Console.WriteLine("?????  " + margin);
+
+                if (isFirstZoom)
+                {
+                    // cropRect parameters here have to be mapped to the originPartPictureImg dimensions
+                    cropRect.X = cropRect.X * xRatio;
+                    cropRect.Y = cropRect.Y * yRatio;
+                    cropRect.Width = cropRect.Width * xRatio;
+                    cropRect.Height = cropRect.Height * yRatio;
+                    isFirstZoom = false;
+                    bmp = new Bitmap(originPartPictureImg);
+                }
+                // cropRect parameters here are just the same with respect to partPictureBox dimensions 
+                else { bmp = new Bitmap(partPictureBox.Image); }
+
+                if (cropRect.Width > cropRect.Height) { cropRect.Height = cropRect.Width; }
+                else { cropRect.Width = cropRect.Height; }
+
+                Bitmap zoomed = new Bitmap(partPictureBox.Width, partPictureBox.Height);
+
+                using (Graphics g = Graphics.FromImage(zoomed))
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+                    g.DrawImage(bmp, new Rectangle(0, 0, zoomed.Width, zoomed.Height), cropRect, GraphicsUnit.Pixel);
+                }
+                partPictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+                partPictureBox.Image = zoomed;
+            }
+        }
+
+        private void paintZoomRect(object sender, PaintEventArgs e)
+        {
+            Pen pen;
+            if (isDrawing) { pen = new Pen(Color.White, 2); }
+            else { pen = new Pen(Color.Transparent, 2); }
+            pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            e.Graphics.DrawRectangle(pen, getZoomRect());
+            e.Dispose();
+        }
+
+        private Rectangle getZoomRect()
+        {
+            return new Rectangle(
+                Math.Min(startPos.X, currentPos.X),
+                Math.Min(startPos.Y, currentPos.Y),
+                Math.Abs(startPos.X - currentPos.X),
+                Math.Abs(startPos.Y - currentPos.Y));
+        }
+
+
+    }
+}
